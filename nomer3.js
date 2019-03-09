41e5c9163d2e2ccf52fe2ed1ff0ffb0d91742b78
fajrizulfikar <fajrizulfikar85@gmail.com>
2019-03-10 00:34:25 +0700

    Fix nomer3

--- nomer3.js
+++ nomer3.js
@@ -3,26 +3,33 @@ function drawImage(number) {
         console.log("Angka harus ganjil!");
     } else {
         let middle = Math.round(number / 2);
-        let minusMiddle = middle - 1;
-        let plusMiddle = middle + 1;
 
-        //outer loop
-        for (let i = 1; i <= number; i++) {
-            //inner loop
-            for (let j = 1; j <= number; j++) {
-                if (j == middle || i == middle) {
-                    document.write('*');
-                } else if ((i != 1 && i != 5) && j == minusMiddle) {
-                    document.write('*');
-                } else if ((i != 1 && i != 5) && j == plusMiddle) {
-                    document.write('*');
-                } else {
-                    document.write('=');
-                }
+        for (let i = 1; i <= number; i += 2) {
+            for (let k = 0; k < ((number - middle) - i / 2); k++) {
+                document.write('=  ');
+            }
+            for (let j = 0; j < i; j++) {
+                document.write('@');
+            }
+            for (let l = 0; l < ((number - middle) - i / 2); l++) {
+                document.write('=  ');
+            }
+            document.write('<br/>');
+        }
+
+        for (let i = number - 2; i >= 1; i -= 2) {
+            for (let k = 0; k < ((number - middle) - i / 2); k++) {
+                document.write('=  ');
+            }
+            for (let j = 0; j < i; j++) {
+                document.write('@');
+            }
+            for (let l = 0; l < ((number - middle) - i / 2); l++) {
+                document.write('=  ');
             }
             document.write('<br/>');
         }
     }
 }
 
-drawImage(5);
+drawImage(7);
